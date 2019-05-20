10f4a498740a079f4066f30a42b13e7a9337cd40
Nick Uhorchak <nick.uhorchak@sofcdo.com>
2019-05-20 12:02:04 -0400

    hospital rank function working

--- R_Prog/week4_assignment.R
+++ R_Prog/week4_assignment.R
@@ -54,3 +54,47 @@ best <- function(state, outcome){
   #return(name)
 }
 
+
+# Write a function called rankhospital that takes three arguments: the 2-character abbreviated name of a
+# state (state), an outcome (outcome), and the ranking of a hospital in that state for that outcome (num).
+# The function reads the outcome-of-care-measures.csv file and returns a character vector with the name
+# of the hospital that has the ranking specified by the num argument
+
+rankhospital <- function(state, outcome, num = "best"){
+  #error handling
+  if(!state %in% outcome_of_care_measures$State){
+    stop("invalid state")
+  }
+  outcome_list <- c("heart attack", "heart failure", "pneumonia")
+  if(!outcome %in% outcome_list){
+    stop("invalid outcome")
+  }
+  #conditional logic
+  if(outcome == "heart attack"){
+    outcome_of_care_measures %>% 
+      select("State", "Hospital.Name", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack") %>% 
+      filter(State == state) %>% 
+      filter(!is.na(Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack)) %>% 
+      arrange(Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack, Hospital.Name) -> name
+  }else if (outcome == "heart failure"){
+    outcome_of_care_measures %>% 
+      select("State", "Hospital.Name", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure") %>% 
+      filter(State == state) %>% 
+      filter(!is.na(Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure)) %>% 
+      arrange(Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure, Hospital.Name) -> name
+  }else{
+    outcome_of_care_measures %>% 
+      select("State", "Hospital.Name", "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia") %>% 
+      filter(State == state) %>% 
+      filter(!is.na(Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia)) %>% 
+      arrange(Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia, Hospital.Name) -> name
+  }
+  if(num=="best"){
+    hospital <- dplyr::first(name$Hospital.Name)
+  }else if(num=="worst"){
+    hospital <- dplyr::last(name$Hospital.Name)
+  }else{
+    hospital <- dplyr::nth(name$Hospital.Name, num)
+  }
+  return(hospital)
+}
